e84d7836af03f5c0df14f3bdd80b8b1843d20758
HajimeKoike <hajimekoike@outlook.jp>
2021-01-07 17:34:10 +0900

    european_call

---
+++ option_pricing/european_call.cc
@@ -0,0 +1,54 @@
+#define ANAL 1
+#define MC 0
+
+#include <bits/stdc++.h>
+
+using namespace std;
+
+void call( double s, double& c, double r, double sigma, int nt, int i, double k ) {
+	double d1 = (log(s/k)+(r+sigma*sigma/2)*i)/(sigma*sqrt(i));
+	double d2 = (log(s/k)+(r-sigma*sigma/2)*i)/(sigma*sqrt(i));
+	c = s * 0.5*( erf( d1/sqrt(2) )+1 ) - exp(-r*(nt-i)) * 0.5*( erf( d2/sqrt(2) )+1 );
+}
+
+int main() {
+#if ANAL
+{
+	double r=0.01;
+	double sigma=0.01;
+	double k=3.00;
+
+	int nt=100+1;
+
+	double s;
+	int i=0;
+	while ( cin>>s ) {
+		double c;
+		call( s, c, r, sigma, nt, i, k );
+		cout << setprecision(10) << c << endl;
+		++i;
+	}
+}
+#endif
+#if MC
+{
+	double r=0.01;
+	double sigma=0.01;
+	double k=3.00;
+
+	double s;
+	int i=0;
+	while ( getline(cin,line) ) {
+		stringstream ss(line);
+		double c=0;
+		for ( int j=0; j<m; ++j ) {
+			getline(ss,s);
+			c+=exp(-r*i)*max(s-k,0);
+		}
+		c/=m;
+		cout << setprecision(10) << c << endl;
+		++i;
+	}
+}
+#endif
+}
