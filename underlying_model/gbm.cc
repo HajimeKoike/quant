44c989c4ecff16728f083d8739c1aef35806997b
HajimeKoike <hajimekoike@outlook.jp>
2021-01-29 02:04:46 +0900

    gbm.cc: updated

--- underlying_model/gbm.cc
+++ underlying_model/gbm.cc
@@ -1,12 +1,9 @@
-#define EIGEN_USE_MKL_ALL
 #define ANAL 0
-#define MC 1
+#define EM 1
 
 #include <bits/stdc++.h>
-#include <Eigen/Dense>
 
 using namespace std;
-using namespace Eigen;
 
 void gbm( double& x0, double& xout, 
 		  int i, double w, 
@@ -14,65 +11,62 @@ void gbm( double& x0, double& xout,
 	xout = x0 * exp( (mu-sigma*sigma/2.)*i + sigma*w );
 }
 
-void gbm_em( VectorXd& xin, VectorXd& xout, 
-			 double dt, VectorXd dw, 
+void gbm_em( vector<double>& xin, vector<double>& xout, 
+			 double dt, vector<double> dw, 
 			 double mu, double sigma, int m ) {
-	assert ( xin.rows()==m && xout.rows()==m && dw.rows()==m );
 	for ( int j=0; j<m; ++j ) 
-		xout(j) = xin(j) + mu*dt*xin(j) + sigma*sqrt(dt)*xin(j)*dw(j);
+		xout[j] = xin[j] + mu*xin[j] * dt + sigma*xin[j]*sqrt(dt) * dw[j];
 }
 
 int main() {
 #if ANAL
 {
-	int seed=42;
-	double mu=0.02;
-	double sigma=0.01;
+	int seed = 42;
+	double mu = 0.02;
+	double sigma = 0.01;
 
-	int nt=100;
+	int nt = 100;
 
-	double x0=1.0;
+	double x0 = 1.0;
 	double xout;
 
 	mt19937 mt(seed);
 	normal_distribution<double> n(0,1);
 
-	cout << setprecision(10) << x0 << endl;
 	for ( int i=1; i<=nt; ++i ) {
 		double w = sqrt(i) * n(mt);
 		gbm(x0,xout,i,w,mu,sigma);
-		cout << setprecision(10) << xout << endl;
 	}
+	cout << setprecision(10) << xout << endl;
 }
 #endif
-#if MC
+#if EM
 {
-	int seed=42;
-	double mu=0.02;
-	double sigma=0.01;
+	int seed = 42;
+	double mu = 0.02;
+	double sigma = 0.01;
 
-	int nt=100;
-	int m=10000;
-	double dt=1.e-2;
+	int nt = 100;
+	int m = 10000;
+	double dt = 1.e-2;
 
 	cin >> m >> dt;
 
-	double x0=1.0;
-	VectorXd x0_vec = x0 * VectorXd::Ones(m);
-	VectorXd xin(m),xout(m);
+	double x0 = 1.0;
+	vector<double> x0_vec(m,x0)
+	vector<double> xin(m),xout(m);
 
 	mt19937 mt(seed);
 	normal_distribution<double> n(0,1);
 
 	xin=x0_vec;
-	cout << setprecision(10) << xin.transpose() << endl;
 	for ( int i=1; i<=int(nt/dt); ++i ) {
-		VectorXd dw(m);
-		for ( int j=0; j<m; ++j ) dw(j) = n(mt);
+		vector<double> dw(m);
+		for ( int j=0; j<m; ++j ) dw[j] = n(mt);
 		gbm_em(xin,xout,dt,dw,mu,sigma,m);
 		xin=xout;
-		cout << setprecision(10) << xin.transpose() << endl;
 	}
+	for ( int j=0; j<m; ++j ) cout << setprecision(10) << xin[j] << endl;
 }
 #endif
 }
